f1363381f689bd597f18e1960b9fc92fc41823a6
yandif <me.yandif@gmail.com>
2024-01-30 11:25:02 +0800

    chore: 使用tsup 打包

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import './styles/index.css';
 export * from './contents';
 export { default } from './layout';
+export { ColorSchemeScript } from '@mantine/core';
---
+++ tsup.config.ts
@@ -0,0 +1,76 @@
+import { promises as fsPromises } from 'fs';
+import path from 'path';
+import postcss from 'postcss';
+import postcssModules from 'postcss-modules';
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  tsconfig: './tsconfig.build.json',
+  entry: ['src/index.ts'],
+  format: 'esm',
+  dts: true,
+  name: 'nextra-theme-infp',
+  outExtension: () => ({ js: '.js' }),
+  external: ['nextra', 'react'],
+  esbuildPlugins: [
+    {
+      name: 'css-module',
+      setup(build): void {
+        build.onResolve(
+          { filter: /\.module\.css$/, namespace: 'file' },
+          (args) => ({
+            path: `${args.path}#css-module`,
+            namespace: 'css-module',
+            pluginData: {
+              pathDir: path.join(args.resolveDir, args.path),
+            },
+          }),
+        );
+        build.onLoad(
+          { filter: /#css-module$/, namespace: 'css-module' },
+          async (args) => {
+            const { pluginData } = args as {
+              pluginData: { pathDir: string };
+            };
+
+            const source = await fsPromises.readFile(
+              pluginData.pathDir,
+              'utf8',
+            );
+
+            let cssModule = {};
+            const result = await postcss([
+              postcssModules({
+                getJSON(_, json) {
+                  cssModule = json;
+                },
+              }),
+            ]).process(source, { from: pluginData.pathDir });
+
+            return {
+              pluginData: { css: result.css },
+              contents: `import "${
+                pluginData.pathDir
+              }"; export default ${JSON.stringify(cssModule)}`,
+            };
+          },
+        );
+        build.onResolve(
+          { filter: /\.module\.css$/, namespace: 'css-module' },
+          (args) => ({
+            path: path.join(args.resolveDir, args.path, '#css-module-data'),
+            namespace: 'css-module',
+            pluginData: args.pluginData as { css: string },
+          }),
+        );
+        build.onLoad(
+          { filter: /#css-module-data$/, namespace: 'css-module' },
+          (args) => ({
+            contents: (args.pluginData as { css: string }).css,
+            loader: 'css',
+          }),
+        );
+      },
+    },
+  ],
+});
