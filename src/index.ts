c1628ed179f9d77266bc06a295be6b0cfcad89c2
yandif <me.yandif@gmail.com>
2024-02-16 12:13:18 +0800

    build: v0.0.8

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import './styles/index.css'; // comment this line if you want to run dev
+import './styles/index.css'; // comment this line if you want to run dev
 export * from './contents';
 export { default } from './layout';
 export { ColorSchemeScript } from '@mantine/core';
